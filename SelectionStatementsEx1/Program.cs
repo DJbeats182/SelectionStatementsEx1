1b195273a3c5220d0996c8192ac872a4fac1e475
DJ Bates <blooviper@gmail.com>
2022-08-08 15:11:58 -0600

    intial commit

---
+++ SelectionStatementsEx1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SelectionStatementsEx1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //------Number-guessing-game-----------------
+            var r = new Random();
+            var favNumber = r.Next(0, 1001);
+            int userInput;
+            
+            do
+            {
+                Console.WriteLine("Pick a number from 1 to 1000");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too low");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too high");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+            } while (userInput != favNumber);
+            
+            
+        }
+    }
+}
